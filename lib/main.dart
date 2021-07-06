0b14fa9f0ca3798f3fc3cf9dfddb68538225041d
Sanoj Raja <pimen07860@gmail.com>
2021-07-06 22:27:55 +0530

    Added error handler on main.dart & fixed orientation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:zineb_el_chat/app/core/constants/app_colors.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Zineb El Chat",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      debugShowCheckedModeBanner: false,
-    ),
+void main() async {
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
+
+  runZonedGuarded<Future<void>>(
+    () async {
+      runApp(
+        GetMaterialApp(
+          title: "Zineb El Chat",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          debugShowCheckedModeBanner: false,
+        ),
+      );
+    },
+    (dynamic error, StackTrace stackTrace) {
+      print('<<----------ERROR--------->> \n$error $stackTrace');
+      Get.defaultDialog(
+        title: 'Unknown Error Occured.',
+        middleText: 'Something went wrong try again later.',
+        middleTextStyle: TextStyle(fontSize: 18),
+        radius: 5,
+        buttonColor: AppColors.facebookIconColor,
+        confirmTextColor: AppColors.textColor1,
+        textConfirm: 'Go Back',
+        onConfirm: () {
+          Get.back();
+          // One back for closing the pop up.
+          // other one for back navigation from the error page.
+          Get.back();
+        },
+      );
+    },
   );
 }
